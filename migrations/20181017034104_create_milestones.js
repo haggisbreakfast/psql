c18b1cd1e850fd3c97fedbc2ecf237f22f84b507
Haggis Breakfast <lj.stewart00@gmail.com>
2018-10-16 21:54:24 -0700

    added primary and foreign keys to milestones table

--- migrations/20181017034104_create_milestones.js
+++ migrations/20181017034104_create_milestones.js
@@ -11,15 +11,3 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return Promise.all([knex.schema.dropTable('milestones')]);
 };
-// exports.up = function(knex, Promise) {
-//   return Promise.all([
-//     knex.schema.createTable('users', function(table) {
-//       table.string('username');
-//       table.string('password');
-//       table.timestamps();
-//     })
-//   ]);
-// };
-// exports.down = function(knex, Promise) {
-//   return Promise.all([knex.schema.dropTable('users')]);
-// };
