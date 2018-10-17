d8895df84c09b815af1380f37a11b2a4107a8606
Haggis Breakfast <lj.stewart00@gmail.com>
2018-10-16 22:08:30 -0700

    fixed primary key

--- migrations/20181017034104_create_milestones.js
+++ migrations/20181017034104_create_milestones.js
@@ -1,9 +1,9 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('milestones', function(table) {
+      table.increments();
       table.string('description');
       table.date('date_achieved');
-      table.integer('id').notNull();
     })
   ]);
 };
